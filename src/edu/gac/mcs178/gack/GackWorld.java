24b3ced7f595f4524263162ee9e80ce7fbd21e8a
Kylar <ky.fenton@yahoo.com>
2025-02-18 10:33:01 -0600

    Create and add new place lund
    New place lund initialized and added it as neighbor to dormitory.  Code was tested and worked.

--- src/edu/gac/mcs178/gack/GackWorld.java
+++ src/edu/gac/mcs178/gack/GackWorld.java
@@ -23,6 +23,8 @@ public class GackWorld extends World {
 		Place offices = new Place("Offices");
 		Place dormitory = new Place("Dormitory");
 		Place pond = new Place("Pond");
+		// Add new place called Lund Center
+		Place lund = new Place("Lund");
 		
 		foodService.addNewNeighbor("down", po);
 		po.addNewNeighbor("south", alumniHall);
@@ -42,6 +44,8 @@ public class GackWorld extends World {
 		lounge.addNewNeighbor("south", offices);
 		computerLab.addNewNeighbor("east", lounge);
 		offices.addNewNeighbor("north", lounge);
+		// Add lund as neighbor to dormitory
+		dormitory.addNewNeighbor("northwest", lund);
 		
 		new AutoPerson("Max", offices, 2);
 		new AutoPerson("Karl", computerLab, 4);
