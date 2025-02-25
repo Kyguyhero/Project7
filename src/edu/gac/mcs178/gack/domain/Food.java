e8c4e725f24a7ee23ebdacbe1710b9c7025db96f
JacTrie <129101052+JacTrie@users.noreply.github.com>
2025-02-25 10:07:43 -0600

    Added food class

--- src/edu/gac/mcs178/gack/domain/Food.java
+++ src/edu/gac/mcs178/gack/domain/Food.java
@@ -1,5 +1,29 @@
 package edu.gac.mcs178.gack.domain;
 
-public class Food {
+import java.util.ArrayList;
+import java.util.List;
 
+import edu.gac.mcs178.gack.Utility;
+public class Food extends Thing{
+    public Food(String title){
+        super(title);
+    }
+    public void beRead(){
+        Person owner = getOwner();
+        if(owner == null){
+            Utility.displayMessage("No one has " + getName());
+        } else {
+			owner.say("I have eaten " + getName());
+		}
+	}
+	
+	public static List<Food> foodIn(Place place) {
+		ArrayList<Food> foodsIn = new ArrayList<Food>();
+		for (Thing thing : place.getContents()) {
+			if (thing instanceof Food) {
+				foodsIn.add((Food) thing);
+			}
+		}
+		return foodsIn;
+	}
 }
