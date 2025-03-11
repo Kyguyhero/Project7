6c625ba317aa5475957524a92edfe1b6b8fc9bcc
Kylar <ky.fenton@yahoo.com>
2025-03-11 00:12:10 -0500

    Variable change
    Made uses variable private int

--- src/edu/gac/mcs178/gack/domain/MagicScroll.java
+++ src/edu/gac/mcs178/gack/domain/MagicScroll.java
@@ -2,7 +2,7 @@ package edu.gac.mcs178.gack.domain;
 
 public abstract class MagicScroll extends Scroll {
 	
-	int uses = 0;
+	private int uses = 0;
 	
 	// Constructor for initiating name of scroll
 	public MagicScroll(String name) {
